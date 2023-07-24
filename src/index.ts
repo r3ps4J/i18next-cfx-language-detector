f9b8ef54e62c3ed036d706a29765398c46b83a2e
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2023-07-24 13:08:44 +0200

    Switch to class

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 import { LanguageDetectorModule } from "i18next";
 
-const languageDetector: LanguageDetectorModule = {
-    type: "languageDetector",
-    detect: () => {
+class LanguageDetector implements LanguageDetectorModule {
+    type = "languageDetector" as const;
+
+    detect() {
         let lng: string | undefined = GetConvar("i18next_lng", GetConvar("locale", ""));
         if (lng == "") {
             lng = undefined;
         }
         return lng;
-    },
-};
+    }
+}
 
-export default languageDetector;
+export default LanguageDetector;
