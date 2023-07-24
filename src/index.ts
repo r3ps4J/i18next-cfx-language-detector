b5d920e0bdc63b06fde39a8d6138d3b573fa7473
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2023-07-24 17:53:03 +0200

    Add detector options

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,33 @@
-import { LanguageDetectorModule } from "i18next";
+import { InitOptions, LanguageDetectorModule, Services } from "i18next";
 
-class LanguageDetector implements LanguageDetectorModule {
-    type = "languageDetector" as const;
+interface DetectorOptions {
+    convar: string;
+}
+
+function getDefaults(): DetectorOptions {
+    return {
+        convar: "i18next_lng",
+    };
+}
+
+class LanguageDetector {
+    static type = "languageDetector" as const;
+
+    private services!: Services;
+    private detectorOptions!: DetectorOptions;
+    private i18nextOptions!: InitOptions<object>;
+
+    init(services: Services, detectorOptions: Partial<DetectorOptions>, i18nextOptions: InitOptions<object>): void {
+        this.services = services;
+        this.detectorOptions = {
+            ...getDefaults(),
+            ...detectorOptions,
+        };
+        this.i18nextOptions = i18nextOptions;
+    }
 
     detect() {
-        let lng: string | undefined = GetConvar("i18next_lng", GetConvar("locale", ""));
+        let lng: string | undefined = GetConvar(this.detectorOptions.convar, GetConvar("locale", ""));
         if (lng == "") {
             lng = undefined;
         }
